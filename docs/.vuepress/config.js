8a2ec5391e4320ec3e93d17e35733598829628d2
hkk <hongkaikao@gmail.com>
2020-02-17 14:02:37 +0800

    2020-0217 1.add Git.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,6 +14,7 @@ module.exports = {
                 'Regex',
                 'SqlThings',
                 'Url',
+                'Git'
             ],
             '/MicroSoft/': [
                 'DotNet',  /* /foo/one.html */
